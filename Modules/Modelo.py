d6c45677ff774907461d4e580ff9cd80d283cd1d
Pukiretsu <angonzalezpa@unal.edu.co>
2023-06-21 16:25:36 -0500

    Implementacion inicial pandas

--- Modules/Modelo.py
+++ Modules/Modelo.py
@@ -1,5 +1,9 @@
-class material:
-    def __init__(self,ID) -> None:
-        self.MaterialID = ID
-    def asignar_modulo_young(self, E) -> None:
-        self.ModuloYoung = E
+import pandas as pd
+import numpy as np
+
+# Definicion de dataframes en el contexto
+MATERIALES = pd.DataFrame({'Nombre': pd.Series(dtype='str'), 'Modulo Young': pd.Series(dtype="float")})
+SECCIONES = pd.DataFrame({'Nombre': pd.Series(dtype='str'), 'Area': pd.Series(dtype="float"), 'Inercia': pd.Series(dtype="float")})
+
+
+

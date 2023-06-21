cef08dbd724e571dc27e671a67fdbb1d6e604b50
Pukiretsu <angonzalezpa@unal.edu.co>
2023-06-21 13:25:26 -0500

    Cambio de directorio

---
+++ Modules/materiales.py
@@ -0,0 +1,5 @@
+class material:
+    def __init__(self,ID) -> None:
+        self.MaterialID = ID
+    def asignar_modulo_young(self, E) -> None:
+        self.ModuloYoung = E
--- materiales.py
+++
@@ -1,5 +0,0 @@
-class material:
-    def __init__(self,ID) -> None:
-        self.MaterialID = ID
-    def asignar_modulo_younh(self, E) -> None:
-        self.ModuloYoung = E
